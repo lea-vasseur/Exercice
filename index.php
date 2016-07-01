74e95cb8f45148ea0059f882f0f777c573aa03a2
lea-vasseur <lea.vasseur@free.fr>
2016-07-01 11:53:54 +0200

    autoload composer

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php 
+if(file_exists('vendor/autoload.php')){
+	require 'vendor/autoload.php';
+}else{
+	echo "vous devez lancer un composer install";
+}
 
 if(file_exists('config.php')){
 	require 'config.php';
